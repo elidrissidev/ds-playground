b01d6496271bccf75c5f7d97a22bcfc315396e12
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-11-09 21:54:58 +0100

    array: wip adding tests

--- array/array_test.c
+++ array/array_test.c
@@ -7,23 +7,33 @@ int main()
     int initial_capacity = 2;
     array_t *arr = array_init(initial_capacity);
 
+    assert(array_capacity(arr) == initial_capacity);
+    assert(array_size(arr) == 0);
+    assert(array_is_empty(arr) == 1);
+
     array_push(arr, 1);
     array_push(arr, 2);
     array_push(arr, 3);
-    array_push(arr, 2);
-    array_push(arr, 2);
 
-    printf("Before:\n");
-    for (int i = 0; i < arr->size; i++) {
-        printf("arr[%d] = %d\n", i, *(arr->items + i));
-    }
+    assert(array_capacity(arr) == initial_capacity * 2);
+    assert(array_size(arr) == 3);
+    assert(array_is_empty(arr) == 0);
 
-    array_remove(arr, 2);
+    array_prepend(arr, 1);
+    array_prepend(arr, 1);
 
-    printf("After:\n");
-    for (int i = 0; i < arr->size; i++) {
-        printf("arr[%d] = %d\n", i, *(arr->items + i));
-    }
+    assert(array_at(arr, 2) == 1);
+    assert(array_at(arr, 3) == 2);
+    assert(array_at(arr, 4) == 3);
+
+    array_insert(arr, 0, 99);
+    array_insert(arr, 2, 99);
+    array_insert(arr, 4, 99);
+    array_remove(arr, 99);
+
+    assert(array_find(arr, 99) == -1);
+
+    // If everything works fine, at the end we should have the first x digits from fib sequence!
 
     array_free(arr);
     return 0;
