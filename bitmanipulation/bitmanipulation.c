986156a2cb89a6b26a37608c074b8da1dc30b3b5
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-12-20 17:05:49 +0100

    bit manipulation: is_bit_set

--- bitmanipulation/bitmanipulation.c
+++ bitmanipulation/bitmanipulation.c
@@ -13,6 +13,12 @@ int flip_bit(int target, int pos)
     return target ^ mask;
 }
 
+int is_bit_set(int target, int pos)
+{
+    int mask = 1 << pos;
+    return (target & mask) >> pos;
+}
+
 int is_even(int x)
 {
     return ~x & 1;
@@ -29,6 +35,11 @@ int main()
     assert(flip_bit(6, 0) == 7); // 6 (110) => 7 (111)
     assert(flip_bit(10, 2) == 14); // 10 (1010) => 14 (1110)
     assert(flip_bit(21, 3) == 29); // 21 (10101) => 29 (11101)
+
+    // Test is_bit_set
+    assert(is_bit_set(6, 0) == 0); // 6 (110)
+    assert(is_bit_set(10, 1) == 1); // 10 (1010)
+    assert(is_bit_set(21, 3) == 0); // 21 (10101)
 
     // Test is_even
     assert(is_even(1) == 0);
